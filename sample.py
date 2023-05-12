1bb7a1bc6be845c66b7cd80942a96ab7abf5ba2a
AnnaYukhno <annet.yukhno@gmail.com>
2023-05-12 08:05:21 +0200

    Banderogoose game (4-days marathon GoIT)

---
+++ sample.py
@@ -0,0 +1,31 @@
+# player_speed = [5, 1]
+
+# if player_rect.bottom >= HEIGHT:
+#     player_speed = random.choice(([1, -1], [-1,-1]))
+
+# if player_rect.top <= 0:
+#     player_speed = random.choice(([-1, 1], [1,1]))
+
+# if player_rect.right >= WIDTH:
+#     player_speed = random.choice(([-1, -1], [-1,1]))
+
+# if player_rect.left <= 0:
+#     player_speed = random.choice(([1, 1], [1,-1]))
+
+# player_rect = player_rect.move(player_speed)
+
+
+
+    # enemy_rect = enemy_rect.move(enemy_move) 
+    # main_display.blit(enemy, enemy_rect)
+
+# main_display.fill(COLOR_BLACK)
+
+#pygame.Surface(player_size)
+# player.fill(COLOR_BLACK)
+
+# pygame.Surface(enemy_size)
+    # enemy.fill(COLOR_BLUE)
+
+#pygame.Surface(bonus_size)
+    #bonus.fill(COLOR_YELLOW)
